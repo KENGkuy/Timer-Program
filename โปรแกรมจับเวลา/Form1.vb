a2ddc9a369d273da34c5d3c61bb867dfa0e8d8d3
KENGkuy <ghyrgkirk@gmail.com>
2022-08-07 15:15:39 +0700

    เป็นโปรแกรมจับเวลา

---
+++ โปรแกรมจับเวลา/Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Start()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Label3.Text = Label3.Text + 1
+        If Label3.Text = 60 Then
+            Label2.Text = Label2.Text + 1
+        End If
+        If Label3.Text = 60 Then
+            Label3.Text = 0
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Timer1.Stop()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Label1.Text = 0
+        Label2.Text = 0
+        Label3.Text = 0
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label1.Text = 0
+        Label2.Text = 0
+        Label3.Text = 0
+    End Sub
+End Class
